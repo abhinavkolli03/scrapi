e63af31d87b86df3c59a7ba283fe66293f6c2c23
abhinavkolli03 <abhinavkolli03@gmail.com>
2023-12-15 15:37:25 -0600

    working files structure

--- gen_scraper.py
+++ gen_scraper.py
@@ -1,20 +1,20 @@
 from playwright.async_api import async_playwright
 import asyncio
 import json
+import os
 
-async def main():
+async def scrape_page(url, selector, document_name):
     async with async_playwright() as pw:
         browser = await pw.chromium.launch(
             headless=False
         )
         
         page = await browser.new_page()
-        await page.goto('https://developer.paypal.com/api/rest/')
+        await page.goto(url)
         await page.wait_for_timeout(5000)
         
-        all_sections = await page.query_selector_all('section[id^="api-section"]')
+        all_sections = await page.query_selector_all(selector)
         data = []
-        print(len(all_sections))
             
         for sect in all_sections:
             
@@ -23,32 +23,25 @@ async def main():
             
             for tag_name in checking_tags:
                 tags = await sect.query_selector_all(tag_name)
-                for i, tag in enumerate(tags, start = 1):
+                for i, tag in enumerate(tags, start=1):
                     text = await tag.inner_text()
                     if text.strip():
                         section_data[f"{tag_name} {i}"] = text
                     
             data.append(section_data)
-            
-        # for product in all_sections:
-        #     result = dict()
-            
-        #     title_el = await product.query_selector('span.a-size-base-plus')
-        #     result['title'] = await title_el.inner_text()
-            
-        #     price_el = await product.query_selector('span.a-price')
-        #     result['price'] = await price_el.inner_text()
-            
-        #     rating_el = await product.query_selector('span.a-icon-alt')
-        #     result['rating'] = await rating_el.inner_text()
-
-        #     data.append(result)
         
-        with open("results.json", "w") as file:
+        if not os.path.exists("scraping_results"):
+            os.makedirs("scraping_results")
+        
+        output_file = os.path.join("scraping_results", f"{document_name}_scraperes.json")
+        with open(output_file, "w") as file:
             json.dump(data, file, indent=4)
             
         await browser.close()
-        
-        
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    doc_url = 'https://stripe.com/docs/api/errors'
+    doc_selector = 'section[id^="api-section"]'
+    document_name = 'stripe'
+    
+    asyncio.run(scrape_page(doc_url, doc_selector, document_name))
